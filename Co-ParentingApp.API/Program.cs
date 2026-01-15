be0b31d4c57d651a010a974514ce9fa7e7481228
Scodia619 <billyjoe2701@gmail.com>
2026-01-15 21:13:22 +0000

    CORS

--- Co-ParentingApp.API/Program.cs
+++ Co-ParentingApp.API/Program.cs
@@ -13,6 +13,16 @@ builder.Services
     .AddCoParentingAppApplication()
     .AddCoParentingAppInfrastructure();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -31,6 +41,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors();
 
 app.UseAuthorization();
 
